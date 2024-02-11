97bf53da4cd24b72223271108abe6580c826ddc1
Alessandro Buonocore <buonocorealessandro98@gmail.com>
2024-02-11 23:59:17 +0100

    stream video

--- app/(site)/[slug]/page.tsx
+++ app/(site)/[slug]/page.tsx
@@ -3,7 +3,11 @@ import { getMedia } from "@/app/libs/prisma-media"
 const Media = async ({ params }: { params: { slug: string } }) => {
     const media = await getMedia(params.slug);
 
-    return <div>{JSON.stringify(media)}</div>
+    return (
+        <video height={400} width={400} controls autoPlay>
+            <source type="video/mp4" src={`https://stream-it.s3.eu-west-2.amazonaws.com/${media?.id}`} />
+        </video>
+    )
 }
 
 export default Media
