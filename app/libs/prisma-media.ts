6988ca518967bc7f138fe47f93c6e7a994bef2d5
Alessandro Buonocore <buonocorealessandro98@gmail.com>
2024-02-11 21:59:17 +0100

    insertMedia to add media to model

--- app/libs/prisma-media.ts
+++ app/libs/prisma-media.ts
@@ -1,5 +1,21 @@
 import prisma from "./prisma-db";
 
+const insertMedia = async (name: string, uploader: string) => {
+    const id = (Math.random() + 1).toString(36).substring(2);
+    // ^^ this will give issues on the long run.
+    // ex: duplicated file names
+
+    const res = await prisma.media.create({
+        data: {
+            id: id,
+            name: name,
+            user: uploader
+        }
+    });
+
+    return {res, id};
+}
+
 const getMedia = async (id: string) => {
     const res = await prisma.media.findUnique({where: {id: id}});
     return res;
@@ -10,4 +26,4 @@ const getAllUserMedias = async (email: string) => {
     return res;
 }
 
-export { getMedia, getAllUserMedias };
+export { getMedia, getAllUserMedias, insertMedia};
