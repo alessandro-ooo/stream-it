dd6e599f0340e4cda7b095c554027dce822d4696
Alessandro Buonocore <buonocorealessandro98@gmail.com>
2024-02-11 21:59:57 +0100

    generate name
    email is placeholder

--- app/api/s3-upload/route.ts
+++ app/api/s3-upload/route.ts
@@ -1,5 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
 import {S3Client, PutObjectCommand} from "@aws-sdk/client-s3";
+import { insertMedia } from "@/app/libs/prisma-media";
 
 const bucket = new S3Client({
     region: process.env.AWS_S3_REGION ?? "",
@@ -13,16 +14,17 @@ const putInBucket = async (file: any, fileName: any) => {
     const fileBuffer = file;
     console.log(fileName);
 
+    const putInORM = await insertMedia(fileName, "buonocorealessandro98@gmail.com");
     const params = {
         Bucket: process.env.AWS_S3_BUCKET,
-        Key: `${fileName}`,
+        Key: `${putInORM.id}`,
         Body: fileBuffer,
         ContentType: "image/jpg"
     }
 
     const command = new PutObjectCommand(params);
     await bucket.send(command);
-    return fileName;
+    return 
 }
 
 export async function POST (request: NextRequest) {
@@ -34,6 +36,7 @@ export async function POST (request: NextRequest) {
             return NextResponse.json({ error: "File is required."}, {status: 400});
         }
 
+        console.log(media);
         const buffer: Buffer = Buffer.from(await media.arrayBuffer());
         const mediaName = await putInBucket(buffer, media.name);
         return NextResponse.json({ success: true, mediaName});
