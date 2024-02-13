8028b320694f0753c6c984c6e200d2dc6954cfaf
Alessandro Buonocore <buonocorealessandro98@gmail.com>
2024-02-14 00:05:48 +0100

    remove old, use component

--- app/(site)/[slug]/page.tsx
+++ app/(site)/[slug]/page.tsx
@@ -1,12 +1,14 @@
+import Player from "@/app/Components/Media/Player";
 import { getMedia } from "@/app/libs/prisma-media"
 
 const Media = async ({ params }: { params: { slug: string } }) => {
     const media = await getMedia(params.slug);
 
     return (
-        <video height={400} width={400} controls autoPlay>
-            <source type="video/mp4" src={`https://stream-it.s3.eu-west-2.amazonaws.com/${media?.id}`} />
-        </video>
+        <Player 
+            URL={media?.id as string} 
+            name={media?.name as string} 
+        />
     )
 }
 
