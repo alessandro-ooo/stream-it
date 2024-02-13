b9b727e18d97cc224208a19c86268dfd9311c1ab
Alessandro Buonocore <buonocorealessandro98@gmail.com>
2024-02-14 00:05:39 +0100

    send data to navbar

--- app/Components/Navbar/Navbar.tsx
+++ app/Components/Navbar/Navbar.tsx
@@ -1,10 +1,19 @@
 
+import { signIn, signOut } from "next-auth/react";
 import { TNavbar } from "./types";
 
 const Navbar = (props: TNavbar) => {
+    const { session } = props;
     return (
         <div>
-            { props.children }
+            {session == null &&
+                <button onClick={() => signIn('google', { callbackUrl: '/' })}>log in with google </button>
+            }
+
+            {session != null &&
+                <button onClick={() => signOut()}>log out</button>
+            }
+            <p>{JSON.stringify(session)}</p>
         </div>
     )
 }
