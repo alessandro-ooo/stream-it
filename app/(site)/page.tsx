e8a2628fda332c4bef7a0197e8976ec85028da26
Alessandro Buonocore <buonocorealessandro98@gmail.com>
2024-02-15 23:42:27 +0100

    add conditional rendering based on session

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,45 +1,75 @@
-"use client"
-import { useEffect } from "react";
 import DnD from "../Components/Drag n drop/dnd";
 import Navbar from "../Components/Navbar/Navbar"
-import { useSession } from "next-auth/react";
 import { getAllUserMedia } from "../libs/prisma-media";
 import Collection from "../Components/Media/Collection";
 import Media from "../Components/Media/Media";
+import { getServerSession } from "next-auth/next"
+import { authOptions } from "../api/auth/[...nextauth]/route";
 
-const Index = () => {
-    // fix this mess
-    const { data: session } = useSession();
-    
-        useEffect(() => {
-            console.log("hook")
-            // if(session != null){
-                const actionGetAllUserMedia = async (email: string) => {
-                const media = await getAllUserMedia(email);
-                return (
-                    <div>
-                        <Navbar session={session}/>
-                        <Collection reactClass="e">
-                            {media.map((vid, i: number) => {
-                                return (
-                                    <Media 
-                                        URL={vid.id}
-                                        name={vid.name}
-                                    />
-                                )
-                            })}
-                        </Collection>
-                    </div>
-                )
-                }
-                actionGetAllUserMedia(session!.user?.email as string);
-            // }
-        },[])
-    
+const Index = async () => {
 
-    if(session == null) {
-        return <Navbar session={session}/>
+    const session = await getServerSession(authOptions)
+
+    if (session != null) {
+        const media = await getAllUserMedia(session.user?.email as string);
+        return (
+            <DnD>
+                <Navbar />
+                <Collection reactClass="e">
+                    {media.map((vid, i: number) => {
+                        return (
+                            <Media
+                                URL={vid.id}
+                                name={vid.name}
+                            />
+                        )
+                    })}
+                </Collection>
+            </DnD>
+        );
     }
+    
+    if(session == null) {
+        return (
+            <DnD>
+                <Navbar />
+                <p>this is the landing page...</p>
+            </DnD>
+        );
+    }
+
+    // fix this mess
+
+    //     useEffect(() => {
+
+    //         console.log("hook")
+    //         // if(session != null){
+    //             const actionGetAllUserMedia = async (email: string) => {
+    //             const media = await getAllUserMedia(email);
+    //             return (
+    //                 <div>
+    //                     <Navbar />
+    //                     <Collection reactClass="e">
+    //                         {media.map((vid, i: number) => {
+    //                             return (
+    //                                 <Media 
+    //                                     URL={vid.id}
+    //                                     name={vid.name}
+    //                                 />
+    //                             )
+    //                         })}
+    //                     </Collection>
+    //                 </div>
+    //             )
+    //             }
+    //             actionGetAllUserMedia(session!.user?.email as string);
+    //         // }
+    //     },[])
+
+
+    // if(session == null) {
+    //     return <Navbar session={session}/>
+    // }
 }
 
 export default Index
