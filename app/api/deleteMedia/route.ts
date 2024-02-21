3f26cf38f9def98288adadc633de48b19ed556b2
Alessandro Buonocore <buonocorealessandro98@gmail.com>
2024-02-21 21:49:48 +0100

    return error if any

--- app/api/deleteMedia/route.ts
+++ app/api/deleteMedia/route.ts
@@ -1,8 +1,14 @@
-import { deleteMedia } from "@/app/libs/prisma-media";
+import { deleteMedia, getMedia } from "@/app/libs/prisma-media";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function DELETE (request: NextRequest) {
     const data: {url: string} = await request.json();
+
+    const media = getMedia(data.url);
+    if(media === null) {
+        return NextResponse.json({status: 403, error: `${data.url} was not found.`});
+    }
+
     const dMedia = await deleteMedia(data.url);
 
     return NextResponse.json({media: dMedia});
