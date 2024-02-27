942c96e6d939a00ed11384dfdf8617a390c1784e
Alessandro Buonocore <buonocorealessandro98@gmail.com>
2024-02-27 22:57:04 +0100

    add new type TCheckbox

--- app/Components/Inputs/types.ts
+++ app/Components/Inputs/types.ts
@@ -6,9 +6,15 @@ export type TInputs = {
     value?: string;
     placeholder?: string;
     label?: string;
+    disabled?: boolean;
     type: "text" | "password"
 }
 
 export type Tbuttons = {
     text: string;
+}
+
+export type TCheckbox = {
+    text: string;
+    checked: boolean
 }
