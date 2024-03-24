d499eae08efb86e62112123a2f461118055d0ac4
Alessandro Buonocore <buonocorealessandro98@gmail.com>
2024-03-24 23:52:19 +0100

    add more elements to the carousel

--- app/Components/Carousel/Carousel.tsx
+++ app/Components/Carousel/Carousel.tsx
@@ -2,30 +2,49 @@ import Image from "next/image";
 
 const Carousel = () => {
     return (
-        <div className="carousel bg-red-400">
+        <div 
+            className="z-10 w-full carousel"
+        >
             <div className="carousel-slide flex flex-row space-x-10">
                 <Image
-                    className="rounded-3xl" 
-                    src="/1.gif" alt="gif" width={300} height={300} 
+                    className="rounded-3xl"
+                    src="/1.gif" alt="gif" width={300} height={220}
                 />
                 <Image
-                    className="rounded-3xl" 
-                    src="/2.gif" alt="gif" width={300} height={300} 
+                    className="rounded-3xl"
+                    src="/2.gif" alt="gif" width={300} height={220}
                 />
                 <Image
-                    className="rounded-3xl" 
-                    src="/3.gif" alt="gif" width={300} height={300} 
+                    className="rounded-3xl"
+                    src="/3.gif" alt="gif" width={300} height={220}
                 />
                 <Image
-                    className="rounded-3xl" 
-                    src="/4.gif" alt="gif" width={300} height={300} 
+                    className="rounded-3xl"
+                    src="/4.gif" alt="gif" width={300} height={220}
                 />
                 <Image
-                    className="rounded-3xl" 
-                    src="/5.gif" alt="gif" width={300} height={300} 
+                    className="rounded-3xl"
+                    src="/5.gif" alt="gif" width={300} height={220}
+                />
+                <Image
+                    className="rounded-3xl"
+                    src="/6.gif" alt="gif" width={300} height={220}
+                />
+                <Image
+                    className="rounded-3xl"
+                    src="/7.gif" alt="gif" width={300} height={220}
+                />
+                <Image
+                    className="rounded-3xl"
+                    src="/8.gif" alt="gif" width={300} height={300}
+                />
+                <Image
+                    className="rounded-3xl"
+                    src="/9.gif" alt="gif" width={300} height={300}
                 />
             </div>
         </div>
+
     )
 }
 
