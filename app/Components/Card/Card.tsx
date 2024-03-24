c90148e5a44ddc75db0fa8dbcfafc22c6785afde
Alessandro Buonocore <buonocorealessandro98@gmail.com>
2024-03-25 00:12:49 +0100

    fix bug that wasn't adding space between elements

--- app/Components/Card/Card.tsx
+++ app/Components/Card/Card.tsx
@@ -6,15 +6,18 @@ const Card = (props: TCard) => {
     const { title, text, img, flex, blurred} = props;
     return(
         <div
-            className={`flex ${flex} pt-24 space-x-20 w-full place-content-center`}
+            className={`flex ${flex} pt-24 w-full h-auto place-content-center`}
         >
             <Image
                 className={`rounded-3xl ${(blurred == true ? "blur" : "")}`}
-                src={`/${img}`}alt="gif" width={400} height={400}
+                src={`/${img}`}
+                alt="gif" 
+                width={400} 
+                height={400}
             />
 
             <div
-                className="flex flex-col w-80"
+                className={`flex flex-col w-96 ${flex == "flex-row" ? "pl-20" : "pr-20"}`}
             >
                 <Text
                     fsize="text-4xl"
