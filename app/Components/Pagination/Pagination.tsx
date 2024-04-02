05f2a3e66a9353d32d21e1d2bac2eb0d65975fac
Alessandro Buonocore <buonocorealessandro98@gmail.com>
2024-04-03 00:46:06 +0200

    implement pagination logic

--- app/Components/Pagination/Pagination.tsx
+++ app/Components/Pagination/Pagination.tsx
@@ -3,41 +3,249 @@ import { TPagination } from "./types"
 
 const Pagination = (props: TPagination) => {
     const { pages, current } = props;
+    const pageZero: number = current-1;
 
-    current = Math.max(1, Math.min(current, pages));
+    if(pages <= 3) {
+        if(current+1 == pages) {
+            return (
+                <div
+                    className="flex flex-row space-x-4 w-full justify-center my-12"
+                >
+                    {pageZero != 0 &&
+                        <Ghost
+                            text={`${current-1}`}
+                        />                   
+                    }
+                    <CTA
+                        text={`${current}`}
+                    />
+                    <Ghost
+                        text={`${pages}`}
+                    />
+                </div>
+            )
+        }
 
+        if(current+2 == pages) {
+            return (
+                <div
+                    className="flex flex-row space-x-4 w-full justify-center my-12"
+                >
+                    <Ghost
+                        text={`<`}
+                    />
+                    <CTA
+                        text={`${current}`}
+                    />
+                    <Ghost
+                        text={`${current+1}`}
+                    />
+                    <Ghost
+                        text={`${current+2}`}
+                    />
+                    <Ghost
+                        text={`>`}
+                    />
+                </div>
+            )
+        }
+    }
+
+    if(current+1 == pages) {
+        return (
+            <div
+                className="flex flex-row space-x-4 w-full justify-center my-12"
+            >
+                <Ghost
+                    text="<"
+                />
+                <Ghost
+                    text={`${current - 1}`}
+                />                   
+                <CTA
+                    text={`${current}`}
+                />
+                <Ghost
+                    text={`${pages}`}
+                />
+                <Ghost
+                    text="..."
+                />
+                <Ghost
+                    text=">"
+                />
+            </div>
+        )
+    }
+
+    if(pages == current) {
+        return(
+            <div
+                className="flex flex-row space-x-4 w-full justify-center my-12"
+            >
+                <Ghost
+                    text="<"
+                />
+
+                <Ghost
+                    text={`${pages - 2}`}
+                />
+
+                <Ghost
+                    text={`${pages - 1}`}
+                />
+
+                <CTA
+                    text={`${pages}`}
+                />
+
+                <Ghost
+                    text="..."
+                />
+
+                <Ghost
+                    text=">"
+                />
+
+            </div>
+        )
+    }
     return(
         <div
             className="flex flex-row space-x-4 w-full justify-center my-12"
         >
-            <Ghost 
+            <Ghost
+                text="<"
+            />
+
+            <CTA
+                text={`${current}`}
+            />
+
+            <Ghost
+                text={`${current+1}`}
+            />
+
+            <Ghost
+                text={`${current+2}`}
+            />  
+
+            <Ghost
+                text="..."
+            />
+
+            <Ghost
+                text={`${pages}`}
+            />
+
+            <Ghost
+                text=">"
+            />
+
+        </div>
+    )
+
+    /*return(
+        <div>
+
+            {pages <= 3 &&
+                
+                <div
+                    className="flex flex-row space-x-4 w-full justify-center my-12"
+                >
+                    <Ghost 
+                        text="<"
+                    />
+
+                    <CTA 
+                        text={`${current}`}
+                    />
+                    {twop != pages &&
+                        <div>
+                            <Ghost 
+                                text={`${current+1}`}
+                            />
+
+                            <Ghost 
+                                text={`${current+2}`}
+                            />
+
+                            <Ghost 
+                                text=">"
+                            />
+                        </div>
+                    }
+
+                    {twop == pages &&
+                        <div>
+                            <Ghost 
+                                text={`${pages}`}
+                            />
+
+                            <Ghost 
+                                text=">"
+                            />
+                        </div>
+                    }
+                </div>
+            
+            }*/
+
+
+
+
+            {/* <Ghost 
                 text="<"
             />
             <CTA 
-                text="1"
+                text={`${current}`}
             />
 
-            <Ghost 
-                text="2"
-            />
+            {pages > 3 && 
 
-            <Ghost 
-                text="3"
-            />
+                <div 
+                    className="flex flex-row space-x-4"
+                >
+                    {c > pages && 
+                        
+                        <div>
+                            <Ghost
+                                text={`${current+1}`}
+                            />
+
+                            <Ghost
+                                text={`${pages}`}
+                            />
+                        </div>
+                    }
+
+                    {c < pages && 
+                        <div>
+                            <Ghost 
+                                text={`${current+1}`}
+                            />
+
+                            <Ghost 
+                                text={`${current+2}`}
+                            />
+                        </div>
+                    }
+                </div>          
+            }
 
             <Ghost 
                 text="..."
             />
 
             <Ghost 
-                text="10"
+                text={`${pages}`}
             />
 
             <Ghost 
                 text=">"
-            />
-        </div>
-    )
+            /> */}
+        // </div>
+    // )
 }
 
 export default Pagination;
