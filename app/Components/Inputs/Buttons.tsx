42454b8f968b39b1c5305e22aa69f7725ab33827
Alessandro Buonocore <buonocorealessandro98@gmail.com>
2024-04-02 23:17:06 +0200

    expand button logic, add CTA and Ghost type of buttons

--- app/Components/Inputs/Buttons.tsx
+++ app/Components/Inputs/Buttons.tsx
@@ -1,12 +1,30 @@
 import { Tbuttons } from "./types";
 
 const CTA = (props: Tbuttons) => {
-    const { text } = props;
+    const { text, fn } = props;
     return (
-        <input
+        <button
             type="submit"
-        />
+            className="py-3 px-4 inline-flex items-center gap-x-2 text-lg font-bold rounded-lg border border-transparent bg-blue-500 text-white hover:bg-blue-700"
+            onClick={fn}
+        >
+            {text}
+        </button>
     )
 }
 
-export default CTA;
+
+const Ghost = (props: Tbuttons) => {
+    const { text, fn } = props;
+    return (
+        <button
+            type="submit"
+            className="py-3 px-4 inline-flex items-center gap-x-2 text-lg font-semibold rounded-lg border border-transparent text-gray-900 hover:bg-gray-100"
+            onClick={fn}
+        >
+            {text}
+        </button>
+    )
+}
+
+export { CTA, Ghost };
