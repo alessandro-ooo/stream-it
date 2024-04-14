99c82260ab818696ef5524d26f20e74591aba9e2
Alessandro Buonocore <buonocorealessandro98@gmail.com>
2024-04-14 21:56:11 +0200

    update style

--- app/(site)/[slug]/page.tsx
+++ app/(site)/[slug]/page.tsx
@@ -1,4 +1,5 @@
 import Player from "@/app/Components/Media/Player";
+import Navbar from "@/app/Components/Navbar/Navbar";
 import { getMedia, hasPassword } from "@/app/libs/prisma-media"
 import { isUserAllowed } from "@/app/libs/prisma-passwords";
 import { cookies } from "next/headers";
@@ -7,21 +8,26 @@ import { redirect } from "next/navigation";
 const Media = async ({ params }: { params: { slug: string } }) => {
     const media = await getMedia(params.slug);
     const hasPass: string | null = await hasPassword(media!.url);
-    
-    if(hasPass != null) {
+
+    if (hasPass != null) {
         const isAllowed: boolean = await isUserAllowed(media!.url, cookies().get('email')?.value as string);
 
-        if(isAllowed == false) {
+        if (isAllowed == false) {
             redirect(`/access?to=${media?.url}`);
         }
     }
 
     return (
         <div>
-            <Player 
-                URL={media?.url as unknown as string} 
-                name={media?.name as string} 
-            />
+            <Navbar />
+            <div
+                className="w-full h-full flex justify-center items-center mt-12"
+            >
+                <Player
+                    URL={media?.url as unknown as string}
+                    name={media?.name as string}
+                />
+            </div>
         </div>
     )
 }
--- app/Components/Media/Player.tsx
+++ app/Components/Media/Player.tsx
@@ -1,18 +1,18 @@
 "use client";
 
 import { TPlayer } from "./types";
+import Text from "../Typography/Paragraphs";
+import { CTA } from "../Inputs/Buttons";
 
 
 const Player = (props: TPlayer) => {
     const { URL, name } = props;
     return (
         <div 
-            className="bg-red-100"
+            className="bg-red-100 flex-col justify-center items-center space-y-2"
         >
             <video
-                className="rounded-2xl"
-                height={100}
-                width={400}
+                className="rounded-2xl h-[38rem]"
                 controls
                 autoPlay>
                 <source
@@ -20,7 +20,35 @@ const Player = (props: TPlayer) => {
                     src={`https://stream-it.s3.eu-west-2.amazonaws.com/${URL}`} 
                     />
             </video>
-            <p>{name}</p>
+
+            <div 
+                className="flex flex-row"
+            >
+                <div 
+                    className="flex flex-col"
+                >
+                    <Text 
+                        fweight="font-semibold"
+                        fsize="text-lg"
+                        extra="text-gray-900"
+                    >
+                        {name}
+                    </Text>
+
+                    <Text 
+                        fweight="font-regular"
+                        fsize="text-normal"
+                        extra="text-gray-900"
+                    >
+                        {"0 views"}
+                    </Text>
+                </div>
+
+                <CTA 
+                    text={"Copy link"}
+                    fn={navigator.clipboard.writeText("this.state.textToCopy")}
+                />
+            </div>
         </div>
     )
 }
