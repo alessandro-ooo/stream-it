777499ab13f5e4dd42132d305dce3630752ebafb
Alessandro Buonocore <buonocorealessandro98@gmail.com>
2024-04-20 15:11:13 +0200

    rework the delete logic

--- app/Components/Media/Settings.tsx
+++ app/Components/Media/Settings.tsx
@@ -8,25 +8,7 @@ type SearchParamProps = {
 const Settings = (props: TSettings, { searchParams }: SearchParamProps) => {
     const { URL } = props;
     const customRef: string = `/?visibility=true&URL=${URL}`;
-    
-    const api_deleteMedia = async (url: string) => {
-
-        const res = await fetch('/api/deleteMedia', {
-            method: "DELETE",
-            body: JSON.stringify({ url })
-        });
-
-        const result = await res.json();
-
-        if(result.status == 403) {
-            return Error(result.status);
-        }
-
-        await fetch('/api/s3-upload', {
-            method: "DELETE",
-            body: JSON.stringify({ url })
-        });
-    }
+    const delmRef: string = `/?delete=true&URL=${URL}`;
 
     return (
         <ul 
@@ -36,9 +18,9 @@ const Settings = (props: TSettings, { searchParams }: SearchParamProps) => {
                 className="p-2 bg-gray-100 text-gray-900 font-semibold text-base w-1/2 flex justify-center p-1 rounded-bl-2xl border border-gray-400 border-solid"
                 id={URL}
                 key={URL}
-                onClick={(mid) => api_deleteMedia(mid.currentTarget.id)}
+                // onClick={(mid) => api_deleteMedia(mid.currentTarget.id)}
             >
-                <Link href="">Delete</Link>
+                <Link href={delmRef}>Delete</Link>
             </li>
             <li 
                 className="p-2 bg-gray-100 text-gray-900 font-semibold text-base w-1/2 flex justify-center p-1 rounded-br-2xl border border-l-0 border-gray-400 border-solid"
