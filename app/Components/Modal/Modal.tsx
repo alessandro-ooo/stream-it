9f124b88e9a55756e0785957e55a1bcb823ef298
Alessandro Buonocore <buonocorealessandro98@gmail.com>
2024-04-20 15:11:21 +0200

    animate modal

--- app/Components/Modal/Modal.tsx
+++ app/Components/Modal/Modal.tsx
@@ -1,10 +1,26 @@
+"use client"
 import { TModal } from "./types";
+import { motion } from "framer-motion"
+
+const variants = {
+    open: { opacity: 1, scale: 1 },
+    closed: { opacity: 0, scale: 0 },
+}
 
 const Modal = (props: TModal) => {
     return (
-        <div className="bg-white rounded-xl border-gray-200 shadow-sm pointer-events-auto w-fit p-8">
-            {props.children}
-        </div>
+        <motion.div 
+            className="flex items-center justify-center space-y-24 z-50 h-full w-full absolute bg-gray-900"
+            initial={{ opacity: 0, scale: 0.5 }}
+            animate={props.status != undefined ? "open" : "closed"}
+            variants={variants}
+            transition={{ duration: 0.2 }}    
+        >
+            <div className="bg-white rounded-xl border-gray-200 shadow-sm pointer-events-auto w-fit p-8 z-50"
+            >
+                {props.children}
+            </div>
+        </motion.div>
     )
 }
 
