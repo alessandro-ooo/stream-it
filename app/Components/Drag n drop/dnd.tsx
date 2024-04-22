50b87d1daad67cdd993a9251807fce52cd89f4d4
Alessandro Buonocore <buonocorealessandro98@gmail.com>
2024-04-22 14:23:12 +0200

    push a notify when the media is being uploaded

--- app/Components/Drag n drop/dnd.tsx
+++ app/Components/Drag n drop/dnd.tsx
@@ -2,10 +2,13 @@
 import { DragEvent, useState } from "react";
 import { TDnD } from "./types";
 import { useRouter } from 'next/navigation'
+import Notify from "../Notify/Notify";
 
 const DnD = (props: TDnD) => {
     const router = useRouter();
     const [isOver, setIsOver] = useState(false);
+    const [isUploaded, setIsUploaded] = useState(false); 
+    const [hasDropped, setHasDropped] = useState(false);
 
     // Define the event handlers
     const handleDragOver = (event: DragEvent<HTMLDivElement>) => {
@@ -20,6 +23,8 @@ const DnD = (props: TDnD) => {
 
     const handleDrop = async (event: DragEvent<HTMLDivElement>) => {
         event.preventDefault();
+        setHasDropped(true);
+
         const files = event.dataTransfer.files;
         const formData = new FormData();
 
@@ -33,17 +38,25 @@ const DnD = (props: TDnD) => {
         const response = await send.json();
 
         if(response.success) {
+            const delay = (ms: number) => new Promise(res => setTimeout(res, ms));
+            await delay(5000);
             router.push(`/${response.id}`)
         }
     }
 
     return (
+
         <div id="dragndrop"
             onDragOver={handleDragOver}
             onDragLeave={handleDragLeave}
             onDrop={handleDrop}
             className={`z-10`}
         >
+            {hasDropped == true &&
+            <div>
+                <Notify text="We're uploading your clip on our servers." trigger={hasDropped} uploading={true} />
+            </div>
+            }
             {props.children}
         </div>
 
