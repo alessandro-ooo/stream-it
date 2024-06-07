14a9d28fb4369d72c8cc6d6f4f0ef70332086df1
Alessandro Buonocore <buonocorealessandro98@gmail.com>
2024-06-07 08:55:38 +0200

    remove logging

--- app/api/deleteMedia/route.ts
+++ app/api/deleteMedia/route.ts
@@ -3,7 +3,6 @@ import { NextRequest, NextResponse } from "next/server";
 
 export async function DELETE (request: NextRequest) {
     const data: { URL: string} = await request.json();
-    console.log("data url", data.URL);
 
     const media = await getMedia(data.URL);
     if(media === null) {
